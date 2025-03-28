1fbaf6795f2826c777bb2ef2dc7a7d6d1e1fa87d
Djeradé Golbé Parfait <70259653+Djerade@users.noreply.github.com>
2025-03-28 14:34:14 +0000

    add: user

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 from app import create_app, db
 from app.models.product import Product
+from app.models.user import User
 
 app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'Product': Product}
-email_validator
+    return {'db': db, 'Product': Product, 'User': User}
+
 if __name__ == '__main__':
     with app.app_context():
         # Créer les tables de la base de données si elles n'existent pas
