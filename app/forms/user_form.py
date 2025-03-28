6908c309e0b66f9702fa168b69607eb999adeb21
Djeradé Golbé Parfait <70259653+Djerade@users.noreply.github.com>
2025-03-28 14:25:53 +0000

    update

--- app/forms/user_form.py
+++ app/forms/user_form.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+import re
 from app.models.user import User
 
 class LoginForm(FlaskForm):
@@ -22,6 +23,11 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('Ce nom d\'utilisateur est déjà utilisé.')
 
     def validate_email(self, email):
+        # Validation basique du format email
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email.data):
+            raise ValidationError('Format d\'email invalide.')
+        
+        # Vérification si l'email existe déjà
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Cet email est déjà utilisé.') 
--- run.py
+++ run.py
@@ -6,7 +6,7 @@ app = create_app()
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'Product': Product}
-
+email_validator
 if __name__ == '__main__':
     with app.app_context():
         # Créer les tables de la base de données si elles n'existent pas
